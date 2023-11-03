12d0f3065bbf498b17b79f6d811d4e1b663d4cb1
zhangchl007 <zhang.arthur@gmail.com>
2023-11-03 18:19:50 +0800

    update sample code

--- src/main/java/com/example/demo/DemoController.java
+++ src/main/java/com/example/demo/DemoController.java
@@ -2,12 +2,21 @@ package com.example.demo;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.concurrent.atomic.AtomicLong;
 
+//# count access website visit and error handling
 @RestController
-public class DemoController{
+public class DemoController {
+	private final AtomicLong counter = new AtomicLong();
 
-    @GetMapping("/hello")
-	public String Hello(){
-		return "Hello Demo Github Action 5";
+	@GetMapping("/")
+	public String index() {
+		return "Hello World!";
 	}
+
+	@GetMapping("/count")
+	public String count() {
+		return "The website Visits Count: " + counter.incrementAndGet();
+	}
+
 }
